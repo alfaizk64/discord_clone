03779aa5f057510be6f1c33b365fd8fd89dc6f63
alfaizk64 <alfaizk64@gmail.com>
2023-09-09 17:56:49 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        lightBlue : "#404EED"
+      },
+      fontFamily: {
+        'ggsans': ['ggsans-normal'],
+        
+      },
+      spacing: {
+        '30' : '120px'
+      }
+    },
     
   },
   plugins: [],
